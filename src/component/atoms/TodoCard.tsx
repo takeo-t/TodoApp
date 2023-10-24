936c7ae45a116d0b9dd1dc4cdfef5d84ed250084
takeo-t <takeo-t@118satellite.com>
2023-10-25 07:56:47 +0900

    AtomicDesign

---
+++ src/component/atoms/TodoCard.tsx
@@ -0,0 +1,43 @@
+import { Card, CardHeader, CardBody, Heading, Stack, StackDivider, Box, Text } from '@chakra-ui/react'
+
+export const TodoCard = () => {
+    return
+    (
+        <>
+        <Card>
+  <CardHeader>
+    <Heading size='md'>Client Report</Heading>
+  </CardHeader>
+
+  <CardBody>
+    <Stack divider={<StackDivider />} spacing='4'>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Summary
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          View a summary of all your clients over the last month.
+        </Text>
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Overview
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          Check out the overview of your clients.
+        </Text>
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Analysis
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          See a detailed analysis of all your business clients.
+        </Text>
+      </Box>
+    </Stack>
+  </CardBody>
+</Card>
+        </>
+    )
+}
