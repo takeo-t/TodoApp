80695abf8b1f805b3190ed76d92d764294afb259
takeo-t <takeo-t@118satellite.com>
2023-10-24 21:07:10 +0900

    first_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Todo } from "./component/Todo";
+
+function App(){
+  const todoSample = {
+    id: 1,
+    text: "Sample Todo Item"
+  };
+
+  const handleDelete = (id: number) => {
+    // ここにToDoを削除するためのロジックを書く
+    console.log(`Todo with id ${id} is to be deleted.`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todo id={todoSample.id} text={todoSample.text} onDelete={handleDelete} />
     </div>
   );
 }
